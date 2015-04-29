8a501b54eb5cc6b1008f52681bb9605d0791400f
cleaf30 <codyleaf30@aol.com>
2015-04-29 16:33:37 -0400

    Update WarGUIDriver.java

--- WarGUIDriver.java
+++ WarGUIDriver.java
@@ -12,7 +12,7 @@ public class WarGUIDriver
 {
    public static void main(String [] args) 
    {
-      JFrame frame = new MyFrame("War");
+      JFrame frame = new MyWarGUIFrame("War");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.pack();
       frame.setVisible(true);
